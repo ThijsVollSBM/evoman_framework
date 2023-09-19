47c6d3fff2baa4d195b8b2611db0061c5e4bcc6a
Thijs Vollebregt <thijs.vollebregt@ext.sbmoffshore.com>
2023-09-19 11:07:13 +0200

    PUSH FOR EMIR

--- optimization_dummy.py
+++ optimization_dummy.py
@@ -25,10 +25,13 @@ def evaluate(env, x):
     return np.array(list(map(lambda y: simulation(env,y), x)))
 
 # update mutation rate
-def update_mutation_rate(sigma, overall_rl, coordinate_rl):
+def update_mutation_rate(mutation_rates, overall_LR, coordinate_LR):
 
-    return sigma * np.exp(overall_rl*np.random.normal()+coordinate_rl*np.random.normal())
+    step_overall_LR = overall_LR*np.random.normal(0,1)
 
+    step_coordinate_LR = coordinate_LR*np.random.normal(0,1,(100,1))
+
+    return mutation_rates * np.exp(step_overall_LR+step_coordinate_LR)
 
 def initialize_population(env, experiment_name, lowerbound, upperbound,
                             population_size, n_vars):
@@ -66,7 +69,6 @@ def initialize_population(env, experiment_name, lowerbound, upperbound,
 
     return population, population_fitness, mutation_step_size, ini_g
 
-
 def save_results(experiment_name, ini_g, best, mean, std):
     # saves results for first pop
 
@@ -76,18 +78,31 @@ def save_results(experiment_name, ini_g, best, mean, std):
         file.write('\n\ngen best mean std')
         file.write('\n'+str(ini_g)+' '+str(round(best,6))+' '+str(round(mean,6))+' '+str(round(std,6))   )
 
-
 def generate_offspring(population ,mutation_step_sizes, learning_rate_overall, learning_rate_coordinate):
 
-    mutation_step_sizes = update_mutation_rate(sigma=mutation_step_sizes, 
-                                                   overall_rl=learning_rate_overall, 
-                                                   coordinate_rl=learning_rate_coordinate)
+    offspring_mutation_rates = update_mutation_rate(mutation_rates=mutation_step_sizes, 
+                                                   overall_LR=learning_rate_overall, 
+                                                   coordinate_LR=learning_rate_coordinate)
 
-    mutations = mutation_step_sizes*np.random.normal(0,1,(100,1))
+    return np.add(population,offspring_mutation_rates*np.random.normal(0,1,(100,1))), mutation_step_sizes
 
-    return np.add(population,mutations), mutation_step_sizes
+def round_robin(population, mutation_step_sizes, population_fitness):
 
+    scores = np.zeros(population_fitness.shape)
 
+    for i in range(len(population)):
+        
+        fitness = population_fitness[i]
+
+        enemies = np.random.randint(low=1, high=len(population_fitness),size=10)
+
+        score = (population_fitness[enemies] < fitness).sum()
+
+        scores[i] = score
+      
+    indices = np.argsort(scores)
+
+    return population[indices[-100:]], mutation_step_sizes[indices[-100:]], population_fitness[indices[-100:]]
 
 def main():
 
@@ -97,7 +112,7 @@ def main():
         os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 
-    experiment_name = 'optimization_test'
+    experiment_name = 'round_robin_selection'
     if not os.path.exists(experiment_name):
         os.makedirs(experiment_name)
 
@@ -121,13 +136,13 @@ def main():
     
     # start writing your own code from here
 
-    generations_max = 30
+    generations_max = 50
     last_best = 0
     lowerbound = -1
     upperbound = 1
     population_size = 100
-    learning_rate_overall = 1/((2*population_size)**0.5)
-    learning_rate_coordinate = 1/((2*(population_size**0.5))**0.5)
+    learning_rate_overall = 1/((2*population_size)**0.5)+1
+    learning_rate_coordinate = 1/((2*(population_size**0.5))**0.5)+0.1
 
     population, population_fitness, mutation_step_sizes, generation = initialize_population(env=env, 
                                                                        experiment_name=experiment_name,
@@ -137,46 +152,51 @@ def main():
                                                                        n_vars=n_vars
                                                                        )
 
-    
+    print(n_vars)
 
     for i in range(generation+1, generations_max):
 
-        best_solution_index = np.argmax(population_fitness)
+        #mean of the population performance
         mean = np.mean(population_fitness)
+
+        #std of the population performance
         std = np.std(population_fitness)
 
-        best_chromosome = population[best_solution_index]
-        best_fitness = population_fitness[best_solution_index]
-        best_mutation_rate = mutation_step_sizes[best_solution_index]
+        #index of the best_solution
+        best_solution_index = np.argmax(population_fitness)
 
+        #best performing individual
+        best_chromosome = population[best_solution_index]
+
+        #fitness of this individual
+        best_fitness = population_fitness[best_solution_index]
+        
+        #mutation_rate of this individual
+        best_mutation_rate = mutation_step_sizes[best_solution_index]
+        
         save_results(experiment_name=experiment_name, ini_g=generation, best=best_fitness, mean=mean, std=std)
 
         #generate offspring (with new mutation step_sizes)
-        offspring, offspring_mutation_step_sizes = generate_offspring(population, learning_rate_overall=learning_rate_overall, 
+        offspring, offspring_mutation_rates = generate_offspring(population, learning_rate_overall=learning_rate_overall, 
                            learning_rate_coordinate=learning_rate_coordinate, mutation_step_sizes=mutation_step_sizes)
     
         #evaluate new solutions
         offspring_fitness = evaluate(env, offspring)
 
-        #cumulative population
-        cum_population = np.vstack((population, offspring))
+        population = np.vstack((population, offspring))
 
         #cumulative fitness
-        cum_fitness = np.concatenate((population_fitness, offspring_fitness))
+        population_fitness = np.concatenate((population_fitness, offspring_fitness))
 
         #cumulative mutation step_sizes
-        cum_step_sizes = np.concatenate((mutation_step_sizes,offspring_mutation_step_sizes))
+        mutation_step_sizes = np.concatenate((mutation_step_sizes,offspring_mutation_rates))
 
 
-        #sort cumulative population and step sizes according to their fitness
-        cum_population_sorted = cum_population[cum_fitness.argsort()]
-        cum_step_sizes_sorted = cum_step_sizes[cum_fitness.argsort()]
-        cum_fitess_sorted = cum_fitness[cum_fitness.argsort()]
+        population, mutation_step_sizes, population_fitness = round_robin(population, mutation_step_sizes, population_fitness)
 
-        #select top 100 chromosomes and their step sizes and population fitness
-        population = cum_population_sorted[population_size:]
-        mutation_step_sizes = cum_step_sizes_sorted[population_size:]
-        population_fitness = cum_fitess_sorted[population_size:]
+
+        generation += 1
+        
         
 
 
